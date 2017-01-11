4f01512da0a3d4f664d0df7ffe6770301f9ef468
laurent Beauvisage <laurentbeauvisage@gmail.com>
2017-01-11 16:36:59 +0100

    Add Check Number

--- base/checkAnswer.php
+++ base/checkAnswer.php
@@ -31,19 +31,19 @@
   function checkAnswer()
   {
     // Create the tests
-
+    // Check 1
     $change_id = new resultElement("Le wariorID doit être votre reflet",function(){
       return $GLOBALS['warriorID'] != 'azertyuiop' ;
     });
 
-
+    // Check 2
     $class_exits = new resultElement("Les <u>classes</u> StartrekWarrior, MarvelWarrior et PokemonWarrior doivent être crées ",function(){
       return (class_exists('StartrekWarrior')
       && class_exists('MarvelWarrior')
       && class_exists('PokemonWarrior'));
     });
 
-
+    // Check 3
     $is_subclass = new resultElement("Les <u>classes</u> StartrekWarrior, MarvelWarrior et PokemonWarrior doivent <u>hériter</u> de Warrior ",function(){
 
       $sWarrior = new StartrekWarrior(1);
@@ -55,7 +55,7 @@
       && is_subclass_of($pWarrior,'Warrior'));
     });
 
-
+    // Check 4
     $has_properties1 = new resultElement("La <u>classe</u> Warrior doit avoir les <u>membres</u> \$name, \$speed, \$life et \$shield, \$imageUrl",function(){
 
       $warrior = new Warrior(4);
@@ -67,6 +67,7 @@
       && property_exists($warrior,'imageUrl'));
     });
 
+    // Check 5
     $has_properties2 = new resultElement("Les <u>classe</u> StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir respectivement les <u>membres</u>  \$mentalPower, \$superPower et \$level",function(){
 
       $sWarrior = new StartrekWarrior(5);
@@ -78,6 +79,7 @@
       && property_exists($pWarrior,'level'));
     });
 
+    // Check 6
     $has_constructors = new resultElement("Les <u>classes</u> Warrior, StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir des <u>constructeurs</u> ",function(){
 
       $warrior = new Warrior(8);
@@ -91,6 +93,7 @@
       && method_exists($pWarrior,'__construct'));
     });
 
+    // Check 7
     $warrior_constructor_value = new resultElement("Le <u>constructeur</u> de Warrior doit prendre en paramètre un \$id et initialiser speed=30, life=100, shield=20",function(){
 
       $warrior = new Warrior(12);
@@ -98,6 +101,7 @@
       return ($warrior->id == 12 && $warrior->speed==30 && $warrior->shield == 20 && $warrior->life ==100);
     });
 
+    // Check 8
     $other_constructor_values = new resultElement("Les <u>constructeurs</u> des sous classes de warrior doivent appeler le <u>constructeur</u> de Warrior et affecter \$mentalPower = 8, \$superPower = 100, \$level = 1, Warrior doit prendre en paramètre un \$id et initialiser speed=30, life=100, shield=20",function(){
 
       $sWarrior = new StartrekWarrior(10);
@@ -111,11 +115,13 @@
     });
 
     $has_weapon = new resultElement("une <u>classe</u> Weapon doit être créée",function(){
+    // Check 9
 
         return (class_exists('Weapon'));
     });
 
     $has_set_weapon = new resultElement("Warrior doit avoir une <u>méthode</u> SetWeapon() qui prend comme <u>argument</u> un Weapon",function(){
+    // Check 10
 
       $warrior = new Warrior(10);
       $sWarrior = new StartrekWarrior(11);
@@ -130,6 +136,7 @@
       return ($warrior->weapon == $we);
     });
 
+    // Check 11
     $has_constructors2 = new resultElement("Weapon doit avoir un <u>constructeur</u> qui initialise \$id ",function(){
 
       $weapon = new Weapon(22,100);
@@ -137,6 +144,7 @@
       return (method_exists($weapon,'__construct') && $weapon->id == 22);
     });
 
+    // Check 12
     $weapon_has_attirbutes = new resultElement("Weapon doivent avoir les <u>attributs</u> \$id, \$strenght et \$imageUrl ",function(){
 
       $weapon = new Weapon(22,100);
@@ -146,6 +154,7 @@
       && property_exists($weapon,'imageUrl'));
     });
 
+    // Check 13
     $has_set_url = new resultElement("Warrior et Weapon doivent avoir une <u>méthode</u> SetUrl(\$url) ",function(){
 
       $warrior = new Warrior(23);
@@ -154,12 +163,13 @@
       return (method_exists($warrior,'SetUrl') && method_exists($weapon,'SetUrl') );
     });
 
-
+    // Check 14
     $has_create_my = new resultElement("La classe BattleField doit avoir une <u>méthode statique</u> createMyWarrior",function(){
       $MethodChecker = new ReflectionMethod('BattleField','createMyWarrior');
       return ($MethodChecker->isStatic() );
     });
 
+    // Check 15
     $has_my_warrior = new resultElement("La createMyWarrior doit <u>instancier</u> un guerrier, lui affecter une arme, une image et le sauvegarder (ie la classe localWarrior contient une <u>méthode</u> save)",function(){
 
       $myWarrior = NULL;
@@ -172,11 +182,13 @@
       return ($myWarrior != NULL  && $myWarrior->weapon!=NULL && $myWarrior->imageUrl!='');
     });
 
+    // Check 16
     $has_create_others = new resultElement("La <u>classe</u> BattleField doit avoir une <u>méthode statique</u> createOtherWarrior",function(){
       $MethodChecker = new ReflectionMethod('BattleField','createOtherWarrior');
       return ($MethodChecker->isStatic() );
     });
 
+    // Check 17
     $has_other_warriors = new resultElement("La createMyWarrior doit <u>instancier</u> un guerrier, lui affecter une arme, une image et le sauvegarder (\ie la <u>classe</u> localWarrior contient une <u>méthode</u> save)",function(){
 
       $otherWarriors = NULL;
