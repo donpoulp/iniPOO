e9a3c54354856f993a0237e759f661038d84d389
laurent Beauvisage <laurentbeauvisage@gmail.com>
2017-01-18 13:56:39 +0100

    Update view + CSS

--- views/fight.blade.php
+++ views/fight.blade.php
@@ -4,6 +4,7 @@
   <title>Warrior Project</title>
   <!-- Fonts -->
   <link href="simple.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
   <script language="JavaScript" type="text/javascript" src="jquery.min.js"></script>
   <script>
     $( document ).ready(function() {
@@ -12,24 +13,31 @@
       var fullpower = 100;
       var addpower = {{ $battleField->myPowerRatio() }}*50;
       var takepower = (1-{{ $battleField->myDefenceRatio() }})*5;
+      var disabled = false;
 
       var powerb = document.getElementById("powerbar");
       var btn = document.getElementById("power-btn");
 
-      btn.onclick=function(){
-        if (power < fullpower){
+      $('#power-btn').on('click', function(){
+          console.log('click');
+          if (power < fullpower){
 
-          console.log(power);
-          power += addpower;
-          powerb.style.width = power + '%';
+            console.log(power);
+            power += addpower;
+            powerb.style.width = power + '%';
 
 
-        }
+          }
 
-        if (power >= fullpower) {
-          document.location.href={{BattleField::getHostJs()}}+'?do=iwin&p1={{$battleField->getMyWarrior()->id}}&p2={{$battleField->getOtherWarrior()->id}}';
-        }
-      }
+          if (power >= fullpower && !disabled) {
+              console.log('update Href iwin');
+              console.log('disabled = ', disabled);
+              disabled = true;
+              $(this).prop('disabled', true);
+              document.location.href={{BattleField::getHostJs()}}+'?do=iwin&p1={{$battleField->getMyWarrior()->id}}&p2={{$battleField->getOtherWarrior()->id}}';
+          }
+      });
+
 
 
       var refreshIntervalId=setInterval(function(){powerdown();},50);
@@ -47,8 +55,11 @@
         powerb.style.width = power + '%';
 
         }
-        if (power <=0) {
-
+        if (power <=0  && !disabled) {
+            console.log('update Href ilost');
+            console.log('disabled = ', disabled);
+            disabled = true;
+            $(this).prop('disabled', true);
             clearInterval(refreshIntervalId);
             document.location.href={{BattleField::getHostJs()}}+'?do=ilost&p1={{$battleField->getMyWarrior()->id}}&p2={{$battleField->getOtherWarrior()->id}}';
         }
@@ -60,13 +71,13 @@
   </script>
 
 </head>
-<body>
+<body class="demo">
   <h1>Warrior Project</h1>
 
   <table>
     <tr>
       <td>
-        <img src="{{ $battleField->getMyWarrior()->imageUrl }}" alt="{{ get_class ($battleField->getMyWarrior()) }}" style="width:300px;">
+        <img src="{{ $battleField->getMyWarrior()->imageUrl }}" alt="{{ get_class ($battleField->getMyWarrior()) }}" style="max-width:300px; max-height: 200px">
         <img src="{{ $battleField->getMyWarrior()->weapon->imageUrl }}" alt="Pan !" style="width:100px;">
       </td>
 
@@ -77,13 +88,15 @@
       </td>
 
       <td>
-        <img src="{{ $battleField->getOtherWarrior()->imageUrl }}" alt="{{ get_class ($battleField->getOtherWarrior()) }}" style="width:300px">
+        <img src="{{ $battleField->getOtherWarrior()->imageUrl }}" alt="{{ get_class ($battleField->getOtherWarrior()) }}" style="max-width:300px; max-height: 200px">
         <img src="{{ $battleField->getOtherWarrior()->weapon->imageUrl }}" alt="Pan !" style="width:100px;">
 
       </td>
     </tr>
   </table>
-  <div id="power-btn">Raaahhhhh !</div>
+  <div class="clearfix center">
+      <button id="power-btn">fight !!!</button>
+  </div>
 
 </body>
 </html>
